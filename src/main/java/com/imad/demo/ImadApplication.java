62cd9da10c300cd53dfed750885c3d81e2302e50
imadham <imad.hamameh@gmail.com>
2020-04-26 16:43:29 +0300

    initialize project

---
+++ src/main/java/com/imad/demo/ImadApplication.java
@@ -0,0 +1,13 @@
+package com.imad.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imad/demo/ImadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imad.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
